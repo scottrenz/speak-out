629d629e4e7ed6efc8ccd4c2c8414b82d7db92cd
Scott Renz <scottrenz@scottrenz.com>
2019-10-16 19:41:22 -0400

    just not host

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,16 @@
 require('dotenv').config();
 
-// module.exports = {
-//   development: {
-//     client: 'postgresql',
-//     connection: {
-//       port: process.env.DB_PORT,
-//       host: process.env.DB_HOST,
-//       database: process.env.DB_DB,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASS
-//     },
-//     production: {
-//       client: 'postgresql',
-//       connection: {
-//         port: process.env.DB_PORT,
-//         host: process.env.DB_HOST,
-//         database: process.env.DB_DB,
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASS
-//         },
-//       pool: {
-//       min: process.env.DATABASE_POOL_MIN,
-//       max: process.env.DATABASE_POOL_MAX,
-//     },
-//     migrations: {
-//       directory: './db/migrations',
-//       tableName: 'knex_migrations',
-//     },
-//     seeds: {
-//       directory: './db/seeds',
-//     },
-//   }
-// }
-
 module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      port: 5432,
+      port: process.env.DB_PORT,
       host: "speakoutstaging.cswncgslisak.us-east-1.rds.amazonaws.com",
-      database: "speakout",
-      user: "postgres",
+      database: process.env.DB_DB,
+      user: process.env.DB_USER,
       password: process.env.DB_PASS
     },
-    pool: {
+      pool: {
       min: process.env.DATABASE_POOL_MIN,
       max: process.env.DATABASE_POOL_MAX,
     },
@@ -56,3 +23,27 @@ module.exports = {
     },
   }
 }
+
+// module.exports = {
+//   development: {
+//     client: 'postgresql',
+//     connection: {
+//       port: 5432,
+//       host: "speakoutstaging.cswncgslisak.us-east-1.rds.amazonaws.com",
+//       database: "speakout",
+//       user: "postgres",
+//       password: process.env.DB_PASS
+//     },
+//     pool: {
+//       min: process.env.DATABASE_POOL_MIN,
+//       max: process.env.DATABASE_POOL_MAX,
+//     },
+//     migrations: {
+//       directory: './db/migrations',
+//       tableName: 'knex_migrations',
+//     },
+//     seeds: {
+//       directory: './db/seeds',
+//     },
+//   }
+// }
