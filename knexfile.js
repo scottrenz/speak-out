3a2c53551a6a754ec292cc11d65c496474bcf8ad
Scott Renz <scottrenz@scottrenz.com>
2019-10-16 13:43:11 -0400

    prod

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,16 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASS
     },
-    pool: {
+    production: {
+      client: 'postgresql',
+      connection: {
+        port: 5432,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DB,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
+      },
+      pool: {
       min: process.env.DATABASE_POOL_MIN,
       max: process.env.DATABASE_POOL_MAX,
     },
